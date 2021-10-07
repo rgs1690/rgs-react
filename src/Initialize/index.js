f22b160b305d4e5aad6a01834cae3aaa601f376e
Grace Sutherland <rgs1690@gmail.com>
2021-10-07 18:47:07 -0500

    format

--- src/Initialize/index.js
+++ src/Initialize/index.js
@@ -20,27 +20,27 @@ function Initialize() {
   return (
     <div className="App">
       <div className="header">
-        <div className="main-container">
-          <h1>{joke.setup}</h1>
-          <h5>{btnText !== 'Get Punchline' ? joke.punchline : ''}</h5>
-          {btnText === 'Get a Joke' || btnText === 'Get Another Joke' ? (
-            <button
-              onClick={getAJoke}
-              className="btn btn-success"
-              type="button"
-            >
-              {btnText}
-            </button>
-          ) : (
-            <button
-              onClick={() => setBtnText('Get Another Joke')}
-              className="btn btn-success"
-              type="button"
-            >
-              {btnText}
-            </button>
-          )}
-        </div>
+        <img
+          src="https://s3-alpha-sig.figma.com/img/a0a8/fab6/9f1975472be5b3d9817f9d3b29440ad3?Expires=1634515200&Signature=ESHFauC5Zca7M3TxgzrQZLvGhE5pQMRA21vluzXdmvU601-Up-x1DVBbGETsZd2vSx4KbYVoxfLJVzo9aLSeFC0rzZFnfOg4-6j9dTM~b90kwJGK0nF~k~UivIcfDJK-vF0lih0MYG5qwcdl~-rEFXu9lmnRxG26F~FbQdjKyirua-tMRbwZX5IqcSrhFu-88NG65iXW2esah1Cak40aymlVBVGz~6YZtEaaguyDbyhqAA7Mmlin-UY9DshaxzbUsm2cL91K4dE4yslDWcE0uiKUXhfqrqCGD0n4OBuYzZbLyZPVzt07qwF0izPg9G7DV5tiEIo~7juOi5fBRj2Wlw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
+          alt="logo"
+        />
+      </div>
+      <div className="main-container">
+        <h1>{joke.setup}</h1>
+        <h5>{btnText !== 'Get Punchline' ? joke.punchline : ''}</h5>
+        {btnText === 'Get a Joke' || btnText === 'Get Another Joke' ? (
+          <button onClick={getAJoke} className="btn btn-success" type="button">
+            {btnText}
+          </button>
+        ) : (
+          <button
+            onClick={() => setBtnText('Get Another Joke')}
+            className="btn btn-success"
+            type="button"
+          >
+            {btnText}
+          </button>
+        )}
       </div>
     </div>
   );
